79ce6faad315f30ad39b675f35f00cd65ac2a680
gbdev25 <gbdev25@gmail.com>
2022-08-03 15:00:47 -0400

    created likes model

---
+++ src/app/models/likes.ts
@@ -0,0 +1,16 @@
+
+
+export class Likes {
+    id: number;
+    user_id: number;
+    post_id: number;
+
+    constructor(id: number,user_id: number, post_id: number){
+        this.id = id;
+        this.user_id = user_id;
+        this.post_id = post_id;
+
+    }
+
+
+}
