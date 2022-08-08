bd3128c85dff9489daefc15d8935631a0ae14bdc
Adam Harbeck <AHarbeck@student.fullsail.edu>
2022-08-08 14:38:44 -0700

    Created Search page and updated styling on follower-card

---
+++ src/app/components/search-card/search-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import User from 'src/app/models/User';
+
+@Component({
+  selector: 'app-search-card',
+  templateUrl: './search-card.component.html',
+  styleUrls: ['./search-card.component.css']
+})
+export class SearchCardComponent implements OnInit {
+
+  @Input() user: User;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
