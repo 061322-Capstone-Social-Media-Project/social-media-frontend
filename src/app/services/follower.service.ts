4be6ef886178a38a8db3f3e3f04e4116bf0403bd
Calvin Post <calvinhenry826@revature.net>
2022-08-09 16:11:31 -0400

    Fixing currentUser in follower service

--- src/app/services/follower.service.ts
+++ src/app/services/follower.service.ts
@@ -12,17 +12,8 @@ export class FollowerService {
 
   followersUrl: string = `${environment.baseUrl}/followers`
   followingUrl: string = `${environment.baseUrl}/following`
-  currentUser: User
 
   constructor(private http: HttpClient, private auth: AuthService) {
-    let user = sessionStorage.getItem('user')
-    if (user) {
-      this.currentUser = JSON.parse(user);
-    }
-  }
-
-  getCurrentUser(): User {
-    return this.auth.currentUser
   }
 
   getFollowing(): Observable<User[]> {
@@ -37,7 +28,7 @@ export class FollowerService {
   addFollowing(followingId: number): Observable<any> {
     return this.http.post(`${this.followingUrl}`,
       {
-        'followerId': this.currentUser.id,
+        'followerId': this.auth.currentUser.id,
         'followingId': followingId
       }, { headers: environment.headers, withCredentials: environment.withCredentials })
   }
@@ -46,7 +37,7 @@ export class FollowerService {
     return this.http.delete(`${this.followingUrl}`, {
       headers: environment.headers, withCredentials: environment.withCredentials,
       body: {
-        'followerId': this.currentUser.id,
+        'followerId': this.auth.currentUser.id,
         'followingId': followingId
       }
     })
