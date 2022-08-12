76b0c54da46b10b9f90c4da3d9d49c2f5f874d14
gbdev25 <gbdev25@gmail.com>
2022-08-12 18:40:21 -0400

    working on component refreshing

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { tap } from 'rxjs';
+import { map, tap } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { NotificationService } from 'src/app/services/notification.service';
 import { Notify } from 'src/app/models/notification';
@@ -10,18 +10,31 @@ import { Notify } from 'src/app/models/notification';
   styleUrls: ['./notification.component.css'],
 })
 export class NotificationComponent implements OnInit {
-  notifications: Notify[];
+  notifications: Notify[] = [];
 
   constructor(private ns: NotificationService, private as: AuthService) {}
 
   ngOnInit(): void {
     this.getNotifications();
+    
   }
 
   getNotifications() {
     this.ns
       .getNotifications(this.as.currentUser.id)
       .pipe(
+        map(data => {
+          return data.filter(notification => {
+            if (
+              notification.status === 'NEW' ||
+              notification.status === 'UNREAD'
+            ) {
+              return true;
+            } else {
+              return false;
+            }
+          });
+        }),
         tap(data => {
           this.notifications = data;
           console.log(this.notifications);
@@ -30,7 +43,26 @@ export class NotificationComponent implements OnInit {
       .subscribe();
   }
 
-  deleteNotification(id: number) {
-    this.ns.deleteNotificationById(id).subscribe(_ => this.ngOnInit());
+  clickBell() {
+    this.notifications.forEach(n => {
+      n.status = 'UNREAD'
+      this.ns.updateNotification(n).subscribe(_ => this.getNotifications());
+    });
+    
+  }
+
+  countNewNotifications() {
+    return this.notifications.filter(notification => {
+      if (notification.status === 'NEW') {
+        return true;
+      } else {
+        return false;
+      }
+    }).length;
+  }
+
+  updateNotification(n: Notify) {
+    n.status = 'READ';
+    this.ns.updateNotification(n).subscribe(_ => this.getNotifications());
   }
 }
