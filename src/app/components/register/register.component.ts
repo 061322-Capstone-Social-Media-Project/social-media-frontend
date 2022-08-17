2a15f44d88b2ca6f45b008b022463937865fe935
Calvin Post <calvinhenry826@revature.net>
2022-08-16 21:19:36 -0400

    Hotfix to allow immediate login after register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -17,18 +17,28 @@ export class RegisterComponent implements OnInit {
     password: new FormControl(''),
     username: new FormControl('')
   })
-  
+
 
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
-  
+
   onSubmit(e: any): void {
     e.preventDefault()
     this.authService.register(this.registerForm.value.firstName || "", this.registerForm.value.lastName || "", this.registerForm.value.email || "", this.registerForm.value.password || "", this.registerForm.value.username || "")
       .subscribe(
         (response) => {
+          this.authService.login(this.registerForm.value.email || "", this.registerForm.value.password || "")
+            .subscribe(
+              (res2) => {
+                this.authService.currentUser = res2
+                this.router.navigate(
+                  ['user-profile'],
+                  { queryParams: { id: res2.id }}
+                )
+              }
+            )
           this.router.navigate(['login'])
         }
       )
